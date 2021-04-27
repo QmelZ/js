aa8169100a9a74c2240b67f20c55ecf710de2219
QmelZ <qmelzthevoid@gmail.com>
2021-04-28 01:28:56 +0300

    fixed

--- src/qmelz/js/Scripts.java
+++ src/qmelz/js/Scripts.java
@@ -1,28 +1,26 @@
 package qmelz.js;
 
-import arc.Core;
-import arc.Events;
-import arc.struct.Seq;
-import mindustry.Vars;
-import mindustry.game.EventType;
-import mindustry.mod.Mod;
+import arc.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.game.*;
+import mindustry.mod.*;
 
 public class Scripts extends Mod{
-
-    public Util util = new Util();
 
     @Override
     public void loadContent(){
         Vars.enableConsole = true;
         try{
-            util.set(Vars.mods.getScripts().getClass(), "blacklist", Seq.with());
+            Reflect.<Seq<String>>get(Vars.mods.getScripts().getClass(), "blacklist").clear();
         }catch(Throwable c){
+            Log.err(c); // might be helpful for debugging
             Events.on(EventType.ClientLoadEvent.class, event -> {
                 Vars.mods.setEnabled(Vars.mods.locateMod("js-unblacklist"), false);
-                Vars.ui.showErrorMessage("your java version isn't compatible with js unblacklist, use java 8 or 11 to fix this issue");
+                Vars.ui.showErrorMessage("an error occured while unblacklisting js\nthe game will now exit to disable js unblacklister");
                 if(Core.scene.getDialog() != null) Core.scene.getDialog().hidden(Core.app::exit);
             });
         }
-        // Vars.mods.getScripts().runConsole("this[\"Util\"] = Vars.mods.locateMod(\"js-unblacklist\").main.util");
     }
 }
